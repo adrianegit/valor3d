2856af0e653405b8e06bd9325fe235685da84c41
Adriane <adrianesouza85@gmail.com>
2026-06-27 23:42:07 -0300

    Sprint 2 - Estrutura base de templates e dashboard

--- sistema/core/views.py
+++ sistema/core/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
 
+
 def home(request):
+
     return render(request, 'core/home.html')
+
+
+
+def dashboard(request):
+
+    return render(request, 'core/dashboard.html')
